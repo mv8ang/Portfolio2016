a140de2f633ad15af2a0ce2ff49fc916f605404a
Mladen V. Angelov <xfk.dgt@gmail.com>
2016-09-18 16:08:10 +0200

    add popover

--- src/common/components/EmailButton/EmailButton.js
+++ src/common/components/EmailButton/EmailButton.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { Badge } from 'react-foundation'
 import CopyToClipboard from 'react-copy-to-clipboard'
+import Popover from 'react-popover'
 import styles from './EmailButton.scss'
+import PopoverContent from './PopoverContent'
 
 class EmailButton extends Component {
   constructor(props) {
@@ -9,28 +11,34 @@ class EmailButton extends Component {
 
     this.state = {
       text: 'mv8.angelov@gmail.com',
+      popoverOpen: false,
     }
   }
 
   _onHover = () => {
     this.setState({text: 'copy to clipboard'})
   }
-
   _onOut = () => {
     this.setState({text: 'mv8.angelov@gmail.com'})
+  }
+  _onClick = () => {
+    this.setState({popoverOpen: !this.state.popoverOpen})
   }
 
   render() {
     return(
-        <a href='//mailto:mv8.angelov@gmail.com'>
-          <CopyToClipboard text='mv8.angelov@gmail.com' onCopy={console.log('copied')}>
-            <Badge color='alert' className={styles.navEmail}
-              onMouseOver={this._onHover}
-              onMouseOut={this._onOut}
-            >
-              {this.state.text}
-            </Badge>
-          </CopyToClipboard>
+        <a href='#' onClick={(e) => {e.preventDefault()}}>
+          <Popover isOpen={this.state.popoverOpen} body={<PopoverContent />} onOuterAction={this._onClick} preferPlace='below'>
+            <CopyToClipboard text='mv8.angelov@gmail.com'>
+              <Badge color='alert' className={styles.navEmail}
+                onMouseOver={this._onHover}
+                onMouseOut={this._onOut}
+                onClick={this._onClick}
+              >
+                {this.state.text}
+              </Badge>
+            </CopyToClipboard>
+          </Popover>
         </a>
       )
     }
