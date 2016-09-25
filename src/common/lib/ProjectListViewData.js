ebcb0281f3b8854cd044a8d28b3d014bab80673f
Mladen V. Angelov <xfk.dgt@gmail.com>
2016-09-25 18:04:56 +0200

    add description to all projects

--- src/common/lib/ProjectListViewData.js
+++ src/common/lib/ProjectListViewData.js
@@ -13,8 +13,8 @@ export const ProjectListViewData = [
     description: [
       'ScienceAtHome is a project developed by Aarhus University. It harnesses the brain-power of thousands of people around the world through gamification, and uses the data to help build a quantum computer. Fascinating!',
       'I was part of a two-man team that turned a hi-fidelity mock-up into an interactive, easy to modify web-app that hosts the project.',
-      ' Immediately after its release, the website was featured in a number of major online media such as Nature, Science, The Verge, Gizmodo and Ars Technica. It performed extremely well under the high load, and given the tight deadline that we had for finishing it, we were ecstatic.',
-      'Working in a close relationship with the amazing team at AU jump-started my love for JS with npm and React.',
+      'Immediately after its release, the website was featured in a number of major online media such as Nature, Science, The Verge, Gizmodo and Ars Technica. It performed extremely well under the high load, and given the tight deadline that we had for finishing it, we were ecstatic.',
+      'Working in a close relationship with the amazing team at AU jump-started my love for JS with Meteor and React, and was an eye-opener to ES6',
     ],
     url: 'https://www.scienceathome.org/',
   },
@@ -29,7 +29,11 @@ export const ProjectListViewData = [
     name: 'regnmedpenge',
     type: 'Website',
     technologies: ['WordPress', 'SCSS'],
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque fuga nostrum, fugit voluptates temporibus, tenetur, voluptate nisi illo perferendis labore aspernatur. Non nisi iusto et consectetur aperiam incidunt illum deleniti!',
+    description: [
+      'Regn med Penge is an app that teaches children the importance of the relationship between numbers and money.',
+      'In this project, I turned a mock-up provided by Marta Okulicz into a custom WordPress theme. The website has simple functionality and CMS, so even though making it included creating a theme from scratch, most of the work was actually done in SCSS - the pages are alive with animations and subtle effects, which we left as nice surprises to the users to discover.',
+      'While writing the stylesheets for RMP, I used a combination of the SMACSS and BEM methodologies and the results were code that’s surprisingly easy to read and maintain, but is at the same time very modular.',
+    ],
     url: 'http://regnmedpenge.dk/',
   },
   {
@@ -43,7 +47,12 @@ export const ProjectListViewData = [
     name: 'hivebeat',
     type: 'Web App',
     technologies: ['React', 'Redux', 'ES6', 'SCSS/modules'],
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae nulla quaerat, voluptas expedita quam aspernatur iusto ea sit fugiat neque! Veniam, optio molestiae, inventore iste asperiores dolorum. Necessitatibus, exercitationem expedita!',
+    description: [
+      'I started work on Hivebeat towards the end of its lifecycle, as a first task at my internship company.',
+      'This is where I mostly kept developing my JS and React skills. Even though I dabbled in the back-end side of things, my work here primarily included working with ES6 and npm modules, making things work in the browser.',
+      'A great addition to my knowledge of the React family was the inclusion of Redux, which is unavoidable when managing the state of a web-app that’s not a Single-Page Application.',
+      'The stylesheets were written in SCSS again, only this time using heavily modified Semantic UI elements as a base. To avoid falling into an insanity pit brim-full of overwrites and hellfire, we used CSS modules. It was the first time I’d used that technology, but its specificity-based and ultra-modular nature grew on me in no time.',
+    ],
     url: 'https://www.hivebeat.com/',
   },
   {
@@ -56,8 +65,13 @@ export const ProjectListViewData = [
     ],
     name: 'voksen',
     type: 'Webshop',
-    technologies: ['Magento', 'JavaScript', 'CSS'],
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet minus unde, tempore dicta facere perferendis voluptatem in inventore enim quaerat molestiae ratione expedita architecto ipsa ab ullam mollitia asperiores veniam.',
+    technologies: ['Magento', 'JavaScript', 'SCSS', 'Gulp'],
+    description: [
+      'Voksen was an adult-webshop that I was assigned with making by my internship company at the time.',
+      'An actual business had to be created from the ground up, so the work was spread across many fields: studying the company and its target group, creating a unique and appealing brand identity based on that, designing with the user in mind, and last but not least – putting everything into code.',
+      'As an e-commerce platform, I chose Magento. I modified the default theme heavily to make it reflect the conclusions of our research findings. The design was provided by Marta Okulicz, who was working on the project with me.',
+      'The new technologies that this project introduced to me were Gulp (because of the simple build process for SCSS) and Git (because Magento is a magical place, where things break at random). For all my resentment at the platform, it was worth the time and effort, if only for bringing those two into my life.',
+    ],
     },
   {
     coverImage: 'https://placeimg.com/880/500/tech',
@@ -70,6 +84,28 @@ export const ProjectListViewData = [
     name: 'portfolio2016',
     type: 'SPA',
     technologies: ['npm', 'React', 'ES6', 'SCSS/modules'],
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eius dicta voluptates nulla inventore at obcaecati rem architecto, eaque incidunt. Cupiditate odio aspernatur saepe ipsum nihil velit quae cum fuga?',
+    description: [
+      'The portfolio that you’re looking at right now.',
+      'I’ve cherry-picked the technologies I enjoy working with the most: React and SCSS modules, compiled with Webpack. Version control is handled with Git.',
+      'For a full code review, please check the link.',
+    ],
+    url: 'https://github.com/mv8ang/Portfolio2016',
+  },
+  {
+    coverImage: 'https://placeimg.com/880/500/tech',
+    images: [
+      'https://placeimg.com/880/500/tech/grayscale',
+      'https://placeimg.com/880/500/tech/grayscale',
+      'https://placeimg.com/880/500/tech/grayscale',
+      'https://placeimg.com/880/500/tech/grayscale',
+    ],
+    name: 'repository',
+    type: 'Miscellaneous',
+    technologies: ['React', 'JS', 'ES6', 'CSS', 'SCSS/modules', 'Gulp', 'Webpack'],
+    description: [
+      'My GitHub account is where you can find all the random bits and pieces of coding that I’ve done for myself, including this portfolio.',
+      'The repositories include cool CSS trickery (css-playground), trying out technologies for the first time (Redux, React-Router, Meteor), as well as templates set-up for later use: bare-bone React and Meteor apps, a CSS framework, and a set-up ready to work with Bower and Gulp.',
+    ],
+    url: 'https://github.com/mv8ang?tab=repositories',
   }
 ]
