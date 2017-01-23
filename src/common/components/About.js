14ecc98372557b70acc8e51509f82bae6292a5c4
Mladen V. Angelov <xfk.dgt@gmail.com>
2017-01-23 19:02:11 +0100

    update About

--- src/common/components/About.js
+++ src/common/components/About.js
@@ -16,15 +16,19 @@ const About = () => (
       </Column>
       <Column small={12} className={styles.aboutContent}>
         <h1 className={styles.pageTitle}>Woah. Who's that handsome devil?</h1>
-        <p>My name is Mladén! I'm coming straight out of Business Academy Aarhus,
-        where I got my Multimedia Design and Communication degree, and am currently
-        finishing my Digital Concept Development BA top-up.</p>
-        <p>I'm a front-end developer with over 2 years of experience, and an affinity
-        for SCSS and JS frameworks. I stick to best practices, and I always try to
-        make the code I push well thought-out and easy to reason about.</p>
+        <p>My name is Mladén and I'm a fresh graduate of Business Academy Aarhus, Denmark.
+        There I got my Multimedia Design and Communication AP degree in 2015, and went on to
+        complete the Digital Concept Development bachelor top-up in January 2017.</p>
+        <p>At heart, I'm a front-end developer with over 3 years of experience, and an affinity
+        for SCSS and JS frameworks. I like poking around and exploring new technologies,
+        tools and frameworks, though lately I've been mostly focused on React and its ecosystem.</p>
+        <p>My studies and a seemingly random series of events have made me join many different
+        projects in the last couple of years. Working on such a variety of teams has
+        taught me to stick to best practices, and always try to make the code I push
+        well thought-out and easy to reason about.</p>
         <p>Make sure to also check out <a href="https://github.com/mv8ang?tab=repositories" target="_blank">my GitHub profile</a>.
         That's where you can find all the random bits and pieces of code that
-        I’ve done for myself throughout the years, including this portfolio.</p>
+        I’ve done for myself throughout the years, including <a href="https://github.com/mv8ang/Portfolio2016" target="_blank">this portfolio</a>.</p>
         <p>The repositories include random CSS trickery
         (<a href="http://mladenangelov.net/testsilo/index.html" target="_blank">css-playground</a>),
         trying out technologies for the first time (Redux, React-Router, Meteor),
